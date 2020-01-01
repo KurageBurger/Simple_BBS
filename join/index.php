2a15c35e740044118f7f9563bec8608d9947eae1
KurageBurger <>
2020-01-02 04:43:07 +0900

    書き直しなどで戻った場合用のテキストを追加しました。

--- join/index.php
+++ join/index.php
@@ -82,6 +82,9 @@ if (!empty($_POST)) {
 			<?php if ($error['image'] === 'type'): ?>
 			<p class="error">* アップロードする画像は.gif .jpg .pngを指定してください</p>
 			<?php endif; ?>
+			<?php if (!empty($error)): ?>
+			<p class="error">* 恐れ入りますが、画像を改めて指定してください</p>
+			<?php endif; ?>
         </dd>
 	</dl>
 	<div><input type="submit" value="入力内容を確認する" /></div>
