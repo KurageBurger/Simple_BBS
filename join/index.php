272682a4a6926141dfc62e9fface7c87c444879d
KurageBurger <>
2020-01-01 20:43:35 +0900

    passwordとemailのフォームにも同様のエラーメッセージを表示するように変更しました

--- join/index.php
+++ join/index.php
@@ -2,6 +2,12 @@
 if ($_POST['name'] === '') {
 	$error['name'] = 'blank';
 }
+if ($_POST['email'] === '') {
+	$error['email'] = 'blank';
+}
+if ($_POST['password'] === '') {
+	$error['password'] = 'blank';
+}
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -26,16 +32,22 @@ if ($_POST['name'] === '') {
 		<dt>ニックネーム<span class="required">必須</span></dt>
 		<dd>
         	<input type="text" name="name" size="35" maxlength="255" value="" />
-			<?php if ($error['name'] === 'blank'): ?>"
-			<p class="error">*ニックネームを入力してください</p>
+			<?php if ($error['name'] === 'blank'): ?>
+			<p class="error">* ニックネームを入力してください</p>
 			<?php endif; ?>
 		</dd>
 		<dt>メールアドレス<span class="required">必須</span></dt>
 		<dd>
         	<input type="text" name="email" size="35" maxlength="255" value="" />
+			<?php if ($error['email'] === 'blank'): ?>
+			<p class="error">* メールアドレスを入力してください</p>
+			<?php endif; ?>
 		<dt>パスワード<span class="required">必須</span></dt>
 		<dd>
         	<input type="password" name="password" size="10" maxlength="20" value="" />
+			<?php if ($error['password'] === 'blank'): ?>
+			<p class="error">* パスワードを入力してください</p>
+			<?php endif; ?>
         </dd>
 		<dt>写真など</dt>
 		<dd>
