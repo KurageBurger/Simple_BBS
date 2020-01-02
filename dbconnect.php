bc4c321fcbb971575315b567acdfa2481bd70870
KurageBurger <>
2020-01-03 04:31:52 +0900

    rootが抜けてアクセスできない状態になっていたので修正しました。

--- dbconnect.php
+++ dbconnect.php
@@ -1,7 +1,7 @@
 <?php
 try {
     $db = new PDO('mysql:dbname=simple_bbs;host+127.0.0.1;
-    charaset=utf8', 'root', '');
+    charaset=utf8', 'root', root);
 } catch(PODException $e) {
     print('DB接続エラー: ' . $e->getMessage());
 }
